634354411336e19632a2a0837f91e538816ea6dd
mahbuba457 <mahbuba-15@diu.edu.bd>
2024-01-20 14:22:59 +0600

    update

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,11 @@ function App() {
     <div>
       <div className="..." >
 
-        <button className="..."  onClick={()=>dispatch(increment())}>Increment</button>
-        <button className="..."  onClick={()=>dispatch(incrementByAmout(6))}>Increment by value</button>
+        <button className="px-3 py-2 rounded-md border-2 border-red-400"
+  onClick={()=>dispatch(increment())}>Increment</button>
+        <button className="text-green-400 text-2xl"  onClick={()=>dispatch(incrementByAmout(6))}>Increment by value</button>
         <div>{count}</div>
-        <button className="..." onClick={()=>dispatch(decrement())}>Decrement</button>
+        <button className="px-3 py-2 rounded-md border-2 border-blue-400" onClick={()=>dispatch(decrement())}>Decrement</button>
       </div>
     </div>
   )
